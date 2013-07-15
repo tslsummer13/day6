93b139f78184f392d233e4f875a3a7b38ea7a8a0
Raghu Betina <rbetina@gmail.com>
2013-07-15 10:06:18 -0500

    Defined our first class

---
+++ klasses.rb
@@ -0,0 +1,10 @@
+class Person
+  attr_accessor :first_name
+  attr_accessor :last_name
+  attr_accessor :hometown
+end
+
+j = Person.new
+j.first_name = "Jeff"
+j.last_name = "Cohen"
+j.hometown = "Evanston"
